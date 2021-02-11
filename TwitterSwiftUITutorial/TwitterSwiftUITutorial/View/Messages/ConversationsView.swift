2dab4e46bc9c5256746265fdec7176e05ac86766
sean <sean@protopie.io>
2021-02-11 13:56:19 +0900

    Building the conversations view

---
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/View/Messages/ConversationsView.swift
@@ -0,0 +1,40 @@
+//
+//  ConversationsView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by KRENGSEAN on 2021/02/11.
+//
+
+import SwiftUI
+
+struct ConversationsView: View {
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+                VStack {
+                    ForEach(0..<10) { _ in
+                        ConversationCell()
+                    }
+                }.padding()
+            }
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "envelope")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding()
+            })
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .padding()
+        }
+    }
+}
+
+struct ConversationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConversationsView()
+    }
+}
