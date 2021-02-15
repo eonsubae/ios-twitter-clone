b96c13dc65ca8ab250ec9557e242c173ccc489bd
eonsubae <eonsubae@naver.com>
2021-02-15 23:51:31 +0900

    Navigation To Profile & Profile Header

---
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/View/Profile/FilterButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  FilterButtonView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by KRITSSEAN on 2021/02/15.
+//
+
+import SwiftUI
+
+struct FilterButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilterButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterButtonView()
+    }
+}
