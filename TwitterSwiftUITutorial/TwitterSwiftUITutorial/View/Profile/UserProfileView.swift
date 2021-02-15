29eedce9a267a2ff6ea2967b823eb92b0ce41550
eonsubae <eonsubae@naver.com>
2021-02-15 23:37:32 +0900

    Create UserProfileView

---
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/View/Profile/UserProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfileView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by KRITSSEAN on 2021/02/15.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfileView()
+    }
+}
