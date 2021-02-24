f4c22ae79523e7be55221f7483a2c2e382d56d59
eonsubae <eonsubae@naver.com>
2021-02-24 23:39:28 +0900

    Finishing Up Login Screen

--- TwitterSwiftUITutorial/TwitterSwiftUITutorial/Utils/Views/CustomSecureField.swift
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/Utils/Views/CustomSecureField.swift
@@ -1,16 +1,15 @@
 //
-//  CustomTextField.swift
+//  CustomSecureField.swift
 //  TwitterSwiftUITutorial
 //
-//  Created by KRITSSEAN on 2021/02/23.
+//  Created by KRITSSEAN on 2021/02/24.
 //
 
 import SwiftUI
 
-struct CustomTextField: View {
+struct CustomSecureField: View {
     @Binding var text: String
     let placeholder: Text
-    let imageName: String
     
     var body: some View {
         ZStack(alignment: .leading) {
@@ -21,15 +20,14 @@ struct CustomTextField: View {
             }
             
             HStack(spacing: 16) {
-                Image(systemName: imageName)
+                Image(systemName: "lock")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 20, height: 20)
                     .foregroundColor(.white)
                 
-                TextField("", text: $text)
+                SecureField("", text: $text)
             }
         }
     }
 }
-
--- TwitterSwiftUITutorial/TwitterSwiftUITutorial/View/Authentication/LoginView.swift
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/View/Authentication/LoginView.swift
@@ -19,24 +19,57 @@ struct LoginView: View {
                     .scaledToFill()
                     .frame(width: 220, height: 100)
                     .padding(.top, 88)
+                    .padding(.bottom, 32)
                 
-                VStack {
-                    CustomTextField(text: $email, placeholder: Text("Email"))
+                VStack(spacing: 20) {
+                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
                         .padding()
                         .background(Color(.init(white: 1, alpha: 0.15)))
                         .cornerRadius(10)
-                        .padding()
                         .foregroundColor(.white)
                     
-                    CustomTextField(text: $password, placeholder: Text("Password"))
+                    CustomSecureField(text: $password, placeholder: Text("Password"))
                         .padding()
                         .background(Color(.init(white: 1, alpha: 0.15)))
                         .cornerRadius(10)
-                        .padding()
                         .foregroundColor(.white)
                 }
+                .padding(.horizontal, 32)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {}, label: {
+                        Text("Forgot Password?")
+                            .font(.footnote)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(.top, 16)
+                            .padding(.trailing, 32)
+                    })
+                }
+                
+                Button(action: {}, label: {
+                    Text("Sign In")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                        .frame(width: 360, height: 50)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                        .padding()
+                })
                 
                 Spacer()
+                
+                HStack {
+                    Text("Don't have an account?")
+                        .font(.system(size: 14))
+                    
+                    Text("Sign Up")
+                        .font(.system(size: 14, weight: .semibold))
+                }
+                .foregroundColor(.white)
+                .padding(.bottom, 40)
             }
         }
         .background(Color(#colorLiteral(red: 0.1124489978, green: 0.6307274699, blue: 0.9512417912, alpha: 1)))
