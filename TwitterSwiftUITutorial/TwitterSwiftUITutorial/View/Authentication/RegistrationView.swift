49d04dee18f726c3e72792675f7dc503cccd533a
eonsubae <eonsubae@naver.com>
2021-02-25 21:16:27 +0900

    Building Registration Screen

--- TwitterSwiftUITutorial/TwitterSwiftUITutorial/View/Authentication/RegistrationView.swift
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/View/Authentication/RegistrationView.swift
@@ -8,8 +8,79 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State var email = ""
+    @State var password = ""
+    @State var fullname = ""
+    @State var username = ""
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Image("plus_photo")
+                    .resizable()
+                    .renderingMode(.template)
+                    .scaledToFill()
+                    .frame(width: 140, height: 140)
+                    .padding(.top, 88)
+                    .padding(.bottom, 16)
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 20) {
+                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    
+                    CustomTextField(text: $fullname, placeholder: Text("Full Name"), imageName: "person")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    
+                    CustomTextField(text: $username, placeholder: Text("Username"), imageName: "person")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    
+                    CustomSecureField(text: $password, placeholder: Text("Password"))
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 32)
+                
+                
+                Button(action: {}, label: {
+                    Text("Sign Up")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                        .frame(width: 360, height: 50)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                        .padding()
+                })
+                
+                Spacer()
+                
+                Button(action: { mode.wrappedValue.dismiss() }, label: {
+                    HStack {
+                        Text("Already have an account?")
+                            .font(.system(size: 14))
+                        
+                        Text("Sign In")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    .foregroundColor(.white)
+                    .padding(.bottom, 40)
+                })
+            }
+        }
+        .background(Color(#colorLiteral(red: 0.1124489978, green: 0.6307274699, blue: 0.9512417912, alpha: 1)))
+        .ignoresSafeArea()
     }
 }
 
