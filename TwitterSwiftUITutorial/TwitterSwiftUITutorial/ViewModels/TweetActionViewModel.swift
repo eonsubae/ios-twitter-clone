abe5fa5e3c64fb9e7424f17fdfa8817523e2202e
eonsubae <eonsubae@naver.com>
2021-03-05 22:24:03 +0900

    Unlike Tweet | Check If User Liked Tweet

--- TwitterSwiftUITutorial/TwitterSwiftUITutorial/ViewModels/TweetActionViewModel.swift
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/ViewModels/TweetActionViewModel.swift
@@ -14,6 +14,7 @@ class TweetActionViewModel: ObservableObject {
     
     init(tweet: Tweet) {
         self.tweet = tweet
+        checkIfUserLikedTweet()
     }
     
     func likeTweet() {
@@ -31,6 +32,26 @@ class TweetActionViewModel: ObservableObject {
     }
     
     func unlikeTweet() {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        let tweetLikesRef = COLLECTION_TWEETS.document(tweet.id).collection("tweet-likes")
+        let userLikesRef = COLLECTION_USERS.document(uid).collection("user-likes")
         
+        COLLECTION_TWEETS.document(tweet.id).updateData(["likes": tweet.likes - 1]) { _ in
+            tweetLikesRef.document(uid).delete { _ in
+                userLikesRef.document(self.tweet.id).delete { _ in
+                    self.didLike = false
+                }
+            }
+        }
+    }
+    
+    func checkIfUserLikedTweet() {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        let userLikesRef = COLLECTION_USERS.document(uid).collection("user-likes").document(tweet.id)
+
+        userLikesRef.getDocument { snapshot, _ in
+            guard let didLike = snapshot?.exists else { return }
+            self.didLike = didLike
+        }
     }
 }
