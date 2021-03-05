e04fb39f0c1e49a6fc21b38be9576bb77d874e64
eonsubae <eonsubae@naver.com>
2021-03-05 21:51:17 +0900

    Building Tweet Detail View

---
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/View/Tweets/TweetActionsView.swift
@@ -0,0 +1,52 @@
+//
+//  TweetActionsView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by KRITSSEAN on 2021/03/05.
+//
+
+import SwiftUI
+
+struct TweetActionsView: View {
+    var body: some View {
+        HStack {
+            Button(action: {}, label: {
+                Image(systemName: "bubble.left")
+                    .font(.system(size: 16))
+                    .frame(width: 32, height: 32)
+            })
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "arrow.2.squarepath")
+                    .font(.system(size: 16))
+                    .frame(width: 32, height: 32)
+            })
+            
+            Spacer()
+
+            Button(action: {}, label: {
+                Image(systemName: "heart")
+                    .font(.system(size: 16))
+                    .frame(width: 32, height: 32)
+            })
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "bookmark")
+                    .font(.system(size: 16))
+                    .frame(width: 32, height: 32)
+            })
+        }
+        .foregroundColor(.gray)
+        .padding(.horizontal)
+    }
+}
+
+struct TweetActionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetActionsView()
+    }
+}
