73190673a8f06ee2fd1cc7dc0b3ac8a9c1f92de0
eonsubae <eonsubae@naver.com>
2021-03-14 17:18:00 +0900

    Refactors & Polishing UI

--- TwitterSwiftUITutorial/TwitterSwiftUITutorial/View/Messages/MessageView.swift
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/View/Messages/MessageView.swift
@@ -21,6 +21,8 @@ struct MessageView: View {
                     .clipShape(ChatBubble(isFromCurrentUser: true))
                     .foregroundColor(.white)
                     .padding(.horizontal)
+                    .padding(.leading, 100)
+                    .padding(.trailing, 16)
             } else {
                 HStack(alignment: .bottom) {
                     KFImage(URL(string: message.user.profileImageUrl))
@@ -34,17 +36,13 @@ struct MessageView: View {
                         .background(Color(.systemGray5))
                         .clipShape(ChatBubble(isFromCurrentUser: false))
                         .foregroundColor(.black)
-                }.padding(.horizontal)
+                }
+                .padding(.horizontal)
+                .padding(.trailing, 100)
+                .padding(.leading, 16)
                 Spacer()
             }
             
         }
-    }
-}
-
-
-struct MessageView_Previews: PreviewProvider {
-    static var previews: some View {
-        MessageView(message: MOCK_MESSAGES[0])
     }
 }
